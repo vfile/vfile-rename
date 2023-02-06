261f1df6b3c9eee7db03a09493434675ee6a071a
Titus Wormer <tituswormer@gmail.com>
2023-02-06 14:08:06 +0100

    Refactor code-style
    *   Add more docs to JSDoc
    *   Add support for `null` in input of API types

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 /**
  * @typedef {import('vfile').VFileCompatible} VFileCompatible
  * @typedef {import('vfile').VFileOptions} VFileOptions
- *
+ */
+
+/**
  * @typedef SpecAffix
- * @property {string} [prefix]
+ *   Define prepending and/or appending.
+ * @property {string | null | undefined} [prefix]
  *   Substring to prepend in front of the field.
- * @property {string} [suffix]
+ * @property {string | null | undefined} [suffix]
  *   Substring to append after the field.
  *
  * @typedef Spec
  *   An object describing path properties to values.
+ *
  *   For each property in spec, if its value is string, the value of the path
  *   property on the given file is set.
  *   If the value is object, it can have a prefix or suffix key, the value of
  *   the path property on the given file is prefixed and/or suffixed.
- * @property {VFileOptions['path']|SpecAffix} [path]
- * @property {VFileOptions['basename']|SpecAffix} [basename]
- * @property {VFileOptions['stem']|SpecAffix} [stem]
- * @property {VFileOptions['extname']|SpecAffix} [extname]
- * @property {VFileOptions['dirname']|SpecAffix} [dirname]
+ * @property {VFileOptions['basename'] | SpecAffix} [basename]
+ *   Change basename (`'index.min.js'`).
+ * @property {VFileOptions['dirname'] | SpecAffix} [dirname]
+ *   Change dirname (`'~'`).
+ * @property {VFileOptions['extname'] | SpecAffix} [extname]
+ *   Change extname (`'.js'`).
+ * @property {VFileOptions['path'] | SpecAffix} [path]
+ *   Change file path (`'~/index.min.js'`).
+ * @property {VFileOptions['stem'] | SpecAffix} [stem]
+ *   Change stem (`'index.min'`).
  *
- * @typedef {'path' | 'basename' | 'stem' | 'extname' | 'dirname'} SpecKey
- *
- * @typedef {(file: VFile) => VFile} Move
+ * @typedef {'basename' | 'dirname' | 'extname' | 'path' | 'stem'} Field
+ *   Fields related to paths.
+ */
+
+/**
+ * @callback Move
  *   When given something, returns a vfile from that, and changes its path
  *   properties.
  *
- *   *   if there is no bound rename (it’s `null` or `undefined`), makes sure
- *       `file` is a `VFile`
+ *   *   if there is no bound rename (it’s `null` or `undefined`), does nothing.
  *   *   otherwise, if the bound rename is a normal string starting with a dot
  *       (`.`), sets `file.extname`
  *   *   otherwise, if the bound rename is a normal string, sets `file.basename`
@@ -35,8 +46,15 @@
  *       performed
  *   *   otherwise, if the bound rename is an object, renames according to the
  *       `Spec`
- * @typedef {(file: VFile) => any} AnyMove
- * @typedef {string|AnyMove|Spec|Array<string|AnyMove|Spec>} Renames
+ * @param {VFile} file
+ *   File to change.
+ * @returns {void}
+ *   Nothing.
+ */
+
+/**
+ * @typedef {string | Move | Spec | Array<string | Move | Spec>} Renames
+ *   How to rename.
  */
 
 import {VFile} from 'vfile'
@@ -46,19 +64,23 @@ const own = {}.hasOwnProperty
 // Order of renaming properties.
 // See <https://github.com/vfile/vfile/blob/7339f82/lib/index.js#L58>
 // Other properties are invalid.
+/** @type {Array<Field>} */
 const order = ['path', 'basename', 'stem', 'extname', 'dirname']
 
+// To do: next major: only allow actual vfiles.
+// To do: next major: don’t return given file.
 /**
  * Rename a file.
  *
- * @param {VFileCompatible} [value]
+ * @param {VFileCompatible | null | undefined} [value]
  *   File to rename.
- * @param {Renames} [renames]
+ * @param {Renames | null | undefined} [renames]
  *   Rename instructions.
  * @returns {VFile}
  *   The renamed `file`.
  */
 export function rename(value, renames) {
+  // @ts-expect-error: `VFile` should support `null`.
   const file = value instanceof VFile ? value : new VFile(value)
   convert(renames)(file)
   return file
@@ -68,14 +90,14 @@ export function rename(value, renames) {
  * Create a function (the move) from `renames`, that when given a file changes
  * its path properties.
  *
- * @param {Renames} [renames]
+ * @param {Renames | null | undefined} [renames]
  *   Rename instructions.
  * @returns {Move}
  *   A move.
  */
 export function convert(renames) {
   if (renames === null || renames === undefined) {
-    return identity
+    return nothing
   }
 
   if (typeof renames === 'function') {
@@ -100,11 +122,11 @@ export function convert(renames) {
  * @returns {Move}
  */
 function specFactory(spec) {
-  /** @type {Array<SpecKey>} */
+  /** @type {Array<Field>} */
   const props = []
   /** @type {Array<Move>} */
   const moves = []
-  /** @type {SpecKey} */
+  /** @type {Field} */
   let prop
 
   // Fail on non-path props.
@@ -122,6 +144,7 @@ function specFactory(spec) {
 
   // Create moves for all specs.
   props.sort(sort)
+
   let index = -1
 
   while (++index < props.length) {
@@ -145,8 +168,12 @@ function specFactory(spec) {
 }
 
 /**
- * @param {Array<string|Move|Spec>} renames
+ * Convert renames into moves.
+ *
+ * @param {Array<string | Move | Spec>} renames
+ *   Renames.
  * @returns {Array<Move>}
+ *   Moves.
  */
 function convertAll(renames) {
   /** @type {Array<Move>} */
@@ -161,12 +188,15 @@ function convertAll(renames) {
 }
 
 /**
+ * Create a move from multiples moves.
+ *
  * @param {Array<Move>} changes
+ *   Moves.
  * @returns {Move}
+ *   Move.
  */
 function allFactory(changes) {
   return all
-
   /** @type {Move} */
   function all(file) {
     const history = file.history.concat()
@@ -177,64 +207,81 @@ function allFactory(changes) {
     }
 
     // Clean history to only include one changed path.
-    file.history = history.concat(file.path)
-
-    return file
+    file.history = [...history, file.path]
   }
 }
 
 /**
- * @param {SpecKey} a
- * @param {SpecKey} b
- * @returns {number}
- */
-function sort(a, b) {
-  return order.indexOf(a) - order.indexOf(b)
-}
-
-/**
- * @param {SpecKey} key
+ * Create a move that sets.
+ *
+ * @param {Field} key
+ *   Field.
  * @param {string} value
+ *   Value to set.
  * @returns {Move}
+ *   Move.
  */
 function setter(key, value) {
   return set
   /** @type {Move} */
   function set(file) {
     file[key] = value
-    return file
   }
 }
 
 /**
- * @param {SpecKey} key
+ * Create a move that prepends.
+ *
+ * @param {Field} key
+ *   Field.
  * @param {string} prefix
+ *   Value to add.
  * @returns {Move}
+ *   Move.
  */
 function prefix(key, prefix) {
   return add
   /** @type {Move} */
   function add(file) {
     file[key] = prefix + file[key]
-    return file
   }
 }
 
 /**
- * @param {SpecKey} key
+ * Create a move that appends.
+ *
+ * @param {Field} key
+ *   Field.
  * @param {string} suffix
+ *   Value to add.
  * @returns {Move}
+ *   Move.
  */
 function suffix(key, suffix) {
   return add
   /** @type {Move} */
   function add(file) {
     file[key] += suffix
-    return file
   }
 }
 
-/** @type {Move} */
-function identity(file) {
-  return file
+/**
+ * Move that doesn’t do anything.
+ *
+ * @type {Move}
+ */
+function nothing() {}
+
+/**
+ * Sort fields on which comes first in `order`.
+ *
+ * @param {Field} a
+ *   Left.
+ * @param {Field} b
+ *   Right.
+ * @returns {number}
+ *   Sort.
+ */
+function sort(a, b) {
+  return order.indexOf(a) - order.indexOf(b)
 }
