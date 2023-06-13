c26f823bd4406c661ab1b77551cbd755ecf5e516
Titus Wormer <tituswormer@gmail.com>
2023-06-13 14:43:49 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -5,76 +5,75 @@
 import assert from 'node:assert/strict'
 import path from 'node:path'
 import test from 'node:test'
-import {toVFile as vfile} from 'to-vfile'
+import {toVFile} from 'to-vfile'
 import {rename} from './index.js'
-import * as mod from './index.js'
 
-test('rename', function () {
+test('rename', async function () {
   assert.deepEqual(
-    Object.keys(mod).sort(),
+    Object.keys(await import('./index.js')).sort(),
     ['convert', 'rename'],
     'should expose the public api'
   )
 
-  let file = vfile('index.js')
+  let file = toVFile('index.js')
   assert.equal(rename(file, 'main.js'), file, 'should return the file')
 
-  file = vfile('index.js')
+  file = toVFile('index.js')
   assert.equal(
     rename(file, 'main.js').path,
     'main.js',
     'should rename the file'
   )
 
-  file = vfile('index.js')
+  file = toVFile('index.js')
   assert.equal(rename(file).path, 'index.js', 'should ignore a missing rename')
 
-  assert.equal(String(rename()), '', 'should create a vfile (#1)')
-  assert.equal(String(rename('!')), '!', 'should create a vfile (#2)')
-  assert.equal(rename({path: '/'}).path, '/', 'should create a vfile (#3)')
+  assert.equal(String(rename()), '', 'should create a toVFile (#1)')
+  assert.equal(String(rename('!')), '!', 'should create a toVFile (#2)')
+  assert.equal(rename({path: '/'}).path, '/', 'should create a toVFile (#3)')
 
-  file = vfile('index.js')
+  file = toVFile('index.js')
   assert.equal(rename(file, '.ts').path, 'index.ts', 'should set extname')
 
-  file = vfile('.dot')
+  file = toVFile('.dot')
   assert.equal(rename(file, '.js').path, '.dot.js', 'should support dotfiles')
 
-  file = vfile('index.js')
+  file = toVFile('index.js')
   assert.equal(
     rename(file, move).path,
     'main.js',
     'should support a function (#1)'
   )
 
-  file = vfile()
+  file = toVFile()
   assert.equal(
     rename(file, move).path,
     'main',
     'should support a function (#2)'
   )
 
-  file = vfile('index.js')
+  file = toVFile('index.js')
   assert.equal(
     rename(file, {stem: 'main'}).path,
     'main.js',
     'should support a spec (#1)'
   )
 
-  file = vfile('index.js')
+  file = toVFile('index.js')
   assert.equal(
     rename(file, {stem: 'readme', extname: '.md'}).path,
     'readme.md',
     'should support a spec (#2)'
   )
 
-  file = vfile({basename: 'index.js', dirname: 'example'})
+  file = toVFile({basename: 'index.js', dirname: 'example'})
   assert.equal(
     rename(file, {stem: {suffix: '-1'}, dirname: {prefix: 'an-'}}).path,
     path.join('an-example', 'index-1.js'),
     'should support a spec (#3)'
   )
 
-  file = vfile('main.md')
+  file = toVFile('main.md')
   assert.equal(
     rename(file, ['readme.htm', {stem: 'index', extname: {suffix: 'l'}}]).path,
     'index.html',
@@ -84,7 +83,7 @@ test('rename', function () {
   assert.throws(
     function () {
       // @ts-expect-error runtime.
-      rename(vfile(), 1)
+      rename(toVFile(), 1)
     },
     /Expected function, string, array, or object as renames/,
     'should fail when renaming non-path properties'
@@ -93,7 +92,7 @@ test('rename', function () {
   assert.throws(
     function () {
       // @ts-expect-error runtime.
-      rename(vfile(), {other: '!'})
+      rename(toVFile(), {other: '!'})
     },
     /Cannot rename `other`: it’s not a path property/,
     'should fail when renaming non-path properties'
@@ -101,7 +100,7 @@ test('rename', function () {
 
   assert.throws(
     function () {
-      rename(vfile(), {extname: '.js'})
+      rename(toVFile(), {extname: '.js'})
     },
     /Setting `extname` requires `path` to be set too/,
     'should fail when partially renaming pathless files'
